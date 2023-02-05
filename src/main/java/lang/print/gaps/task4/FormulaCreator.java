1fe41f8a246af346f3efb74bd25e07025a4db250
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-02-06 04:33:41 +0500

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        Scanner kiritish= new Scanner(System.in);
+        int a,b;
+        double c;
+        a= kiritish.nextInt();
+        b= kiritish.nextInt();
+        c=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        System.out.println(c);
     }
 }
